60919eb9f96537291a5a657003b88d4ba7a91fb8
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-06-12 20:39:06 +0600

    Speaker Done

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -4,11 +4,32 @@ import { Container, Title } from './common';
 
 import './speaker.css';
 
+const speakers = [
+  { name: 'Shahed Karim', title: 'CEO of Petropole', profile: 1 },
+  { name: 'Shahed Karim', title: 'CEO of Petropole', profile: 1 },
+  { name: 'Shahed Karim', title: 'CEO of Petropole', profile: 1 },
+  { name: 'Shahed Karim', title: 'CEO of Petropole', profile: 1 },
+  { name: 'Shahed Karim', title: 'CEO of Petropole', profile: 1 },
+  { name: 'Shahed Karim', title: 'CEO of Petropole', profile: 1 },
+  { name: 'Shahed Karim', title: 'CEO of Petropole', profile: 1 },
+  { name: 'Shahed Karim', title: 'CEO of Petropole', profile: 1 }
+]
+
 export default () => (
   <div className="speaker-area">
     <Container>
       <Title title="Speaker" content="People You would love to listen" />
-      <p>Speaker</p>
+      <div className="speaker">
+        { speakers.map(({ name, title, profile }) => (
+          <div className="single">
+            <img alt={name} src={require(`../assets/speakers/${profile}.jpg`)} />
+            <div className="info">
+              <h4>{name}</h4>
+              <p>{title}</p>
+            </div>
+          </div>
+        )) }
+      </div>
     </Container>
   </div>
 )
