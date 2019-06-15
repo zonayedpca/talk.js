6903a1ab23b35999eee8d268f3fe0892ee54b96e
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-06-15 13:31:27 +0600

    Manual Intro Scroll

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,35 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Navbar } from './common';
 
 import './intro.css';
 import guy from '../assets/images/guy.png';
 
-export default () => (
-  <div className="intro-area">
-    <Container topPad="0">
-      <Navbar />
-      <div className="info">
-        <div className="intro">
-          <p>A Long-awaited Developers Event</p>
-          <h1>Grand JavaScript Meetup</h1>
-          <div className="details">
-            <ul>
-              <li>
-                <p>Location<small>Banani, Dhaka</small></p>
-              </li>
-              <li>
-                <p>Date<small>29 June, 2019</small></p>
-              </li>
-              <li>
-                <p>Registered<small>120 People</small></p>
-              </li>
-            </ul>
+export default () => {
+  const [positionY, setPostionY] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener('scroll', (e) => {
+      setPostionY(window.pageYOffset * 2);
+    })
+  })
+  return (
+    <div className="intro-area">
+      <Container topPad="0">
+        <Navbar />
+        <div className="info">
+          <div className="intro">
+            <p>A Long-awaited Developers Event</p>
+            <h1>Grand JavaScript Meetup</h1>
+            <div className="details">
+              <ul>
+                <li>
+                  <p>Location<small>Banani, Dhaka</small></p>
+                </li>
+                <li>
+                  <p>Date<small>29 June, 2019</small></p>
+                </li>
+                <li>
+                  <p>Registered<small>120 People</small></p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div style={{ transform: `translate3d(0, -${positionY}px, 0)` }} className="animation">
+            <img alt="guy" src={guy} />
           </div>
         </div>
-        <div className="animation">
-          <img alt="guy" src={guy} />
-        </div>
-      </div>
-    </Container>
-  </div>
-)
+      </Container>
+    </div>
+  )
+}
