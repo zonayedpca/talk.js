a98c9fc6d4b9794060dac1b698d0b33a656ccc22
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-06-17 00:18:53 +0600

    Manual push

---
+++ precache-manifest.13806ee6366b32b906b82902991dc0b7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12baa3fb69efa4813b43247c1da6c92f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c341dace72515d2298f",
+    "url": "/static/css/main.0d701621.chunk.css"
+  },
+  {
+    "revision": "9b2b48c53eedda2f773e",
+    "url": "/static/js/2.1de074e4.chunk.js"
+  },
+  {
+    "revision": "3c341dace72515d2298f",
+    "url": "/static/js/main.c6ff40cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "204e9aacc9db0b840dbe8735479d346a",
+    "url": "/static/media/1.204e9aac.jpg"
+  },
+  {
+    "revision": "b1c704388f623fe61f864dd04abd9209",
+    "url": "/static/media/bg.b1c70438.png"
+  },
+  {
+    "revision": "40b0ebd563bd369707079cff4c3f09de",
+    "url": "/static/media/cat.40b0ebd5.gif"
+  },
+  {
+    "revision": "c816d6bb3de36df4da3f2b64f71a014f",
+    "url": "/static/media/thumbnail.c816d6bb.png"
+  },
+  {
+    "revision": "9cca5cb7f012c27d648a816eb2fda2ff",
+    "url": "/static/media/wave.9cca5cb7.svg"
+  }
+]);
