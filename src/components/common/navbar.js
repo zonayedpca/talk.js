1c981e1ba6e79898174a1f3bbb808d184302d787
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-06-21 15:37:49 +0600

    Navbar done

--- src/components/common/navbar.js
+++ src/components/common/navbar.js
@@ -7,5 +7,13 @@ import './navbar.css';
 export const Navbar = () => (
   <div className="navbar">
     <Brand />
+    <ul>
+      <li><a href="#home">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#speaker">Speaker</a></li>
+      <li><a href="#sponsor">Sponsor</a></li>
+      <li><a href="#location">Place</a></li>
+      <li><a className="btn" href="#ticket">Buy Ticket</a></li>
+    </ul>
   </div>
 )
